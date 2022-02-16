67d137fa512776752510d8eb0136aaa5f8c48115
lee <lee@feeditback.co.uk>
2022-02-16 13:35:05 +0000

    fixed position for filter bar, use flex to allow this and also avoid issues with scrolling and reaching the bottom with flex-grow

--- src/components/TellshowContent.vue
+++ src/components/TellshowContent.vue
@@ -1,18 +1,18 @@
 <template>
-  <div
-    class="main-content h-screen overflow-y-auto flex-1 bg-gray-100 md:mt-0 pt-8 md:pt-0 md:pb-0"
-  >
+  <div class="main-content h-screen flex-1 bg-gray-100 md:mt-0 pt-8 md:pt-0 md:pb-0">
     <common-spinner v-if="loading" />
     <template v-else>
-      <div class="bg-gray-800 w-full">
-        <input
-          v-model="textFilter"
-          type="text"
-          class="shadow-sm rounded-lg m-2 focus:ring-2 focus:ring-indigo-200 focus:border-indigo-400 text-center w-6/12"
-          :placeholder="$t('app.textFilter.placeholder')"
-        />
+      <div class="flex flex-col h-full">
+        <div class="bg-gray-800 w-full">
+          <input
+            v-model="textFilter"
+            type="text"
+            class="shadow-sm rounded-lg m-2 focus:ring-2 focus:ring-indigo-200 focus:border-indigo-400 text-center w-6/12"
+            :placeholder="$t('app.textFilter.placeholder')"
+          />
+        </div>
+        <common-list class="overflow-y-auto grow" :items="filteredItems" />
       </div>
-      <common-list :items="filteredItems" />
     </template>
   </div>
 </template>
